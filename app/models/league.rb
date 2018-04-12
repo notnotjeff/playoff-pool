4934501c194e3b5467bf1129c7a9d7d403cfe3cf
Jeff Craig <jeffcraig35@gmail.com>
2018-04-12 01:26:24 -0400

    Add restrictions to adding leagues at the model level so no shennanigans can take place

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,4 +2,10 @@ class League < ApplicationRecord
   has_many :general_managers, dependent: :destroy
   belongs_to :user
   validates :name, length: { maximum: 50 }
+  before_create :have_playoffs_started
+
+  private
+    def have_playoffs_started
+      throw :abort if Round.current_round > 0
+    end
 end
