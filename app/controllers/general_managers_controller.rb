1a9ec2c8e382da02ca143e0a667f18e33d00d319
Jeff Craig <jeffcraig35@gmail.com>
2018-04-15 16:22:22 -0400

    Sort players by total points on gm page

--- app/controllers/general_managers_controller.rb
+++ app/controllers/general_managers_controller.rb
@@ -24,20 +24,20 @@ class GeneralManagersController < ApplicationController
     @gm = GeneralManager.find(params[:id].to_i)
     @league = @gm.league
 
-    @r1_forwards = @gm.roster_players.where(round: 1, position: "F")
-    @r1_defensemen = @gm.roster_players.where(round: 1, position: "D")
+    @r1_forwards = @gm.roster_players.where(round: 1, position: "F").order(round_total: :desc)
+    @r1_defensemen = @gm.roster_players.where(round: 1, position: "D").order(round_total: :desc)
     @r1_goalies = @gm.roster_players.where(round: 1, position: "G")
 
-    @r2_forwards = @gm.roster_players.where(round: 2, position: "F")
-    @r2_defensemen = @gm.roster_players.where(round: 2, position: "D")
+    @r2_forwards = @gm.roster_players.where(round: 2, position: "F").order(round_total: :desc)
+    @r2_defensemen = @gm.roster_players.where(round: 2, position: "D").order(round_total: :desc)
     @r2_goalies = @gm.roster_players.where(round: 2, position: "G")
 
-    @r3_forwards = @gm.roster_players.where(round: 3, position: "F")
-    @r3_defensemen = @gm.roster_players.where(round: 3, position: "D")
+    @r3_forwards = @gm.roster_players.where(round: 3, position: "F").order(round_total: :desc)
+    @r3_defensemen = @gm.roster_players.where(round: 3, position: "D").order(round_total: :desc)
     @r3_goalies = @gm.roster_players.where(round: 3, position: "G")
 
-    @r4_forwards = @gm.roster_players.where(round: 4, position: "F")
-    @r4_defensemen = @gm.roster_players.where(round: 4, position: "D")
+    @r4_forwards = @gm.roster_players.where(round: 4, position: "F").order(round_total: :desc)
+    @r4_defensemen = @gm.roster_players.where(round: 4, position: "D").order(round_total: :desc)
     @r4_goalies = @gm.roster_players.where(round: 4, position: "G")
 
     @r1 = @r2 = @r3 = @r4 = ""
